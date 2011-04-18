d1a8688a14abf49a5a7919a3ea387e4ab635462b
pjgat09@gmail.com <pjgat09@gmail.com@7f936ef5-24ef-13a7-5a25-7df7dec640b6>
2011-04-18 00:41:21 +0000

    Adding temporary arduino code

---
+++ trunk/NESBot3/MMC.h
@@ -0,0 +1,24 @@
+#ifndef MMC_H
+#define MMC_H
+
+#include "WProgram.h"
+#include "SPI.h"
+
+class MMC
+{
+  public:
+    MMC();
+    
+    byte init();
+    byte CMD(byte num, long arg);
+
+    byte read_data(byte* buf, long addr);
+
+    byte write_data(byte* data, long addr);
+
+    //void print_sector();
+  private:
+    SPI spi_port;
+    
+};
+#endif // MMC_H
---
+++ trunk/NESBot4/SPI.h
@@ -0,0 +1,29 @@
+#ifndef SPI_H
+#define SPI_H
+
+#define MOSIP 11       // Arduino pin 11
+#define MISOP 12       // Arduino pin 12
+#define SCLKP 13       // Arduino pin 13
+#define CSP 10         // Arduino analog pin 5
+#define SSP 10         // Arduino pin 10 
+
+
+#include "WProgram.h"
+
+class SPI
+{
+  public:
+    SPI();
+
+    byte send_data(volatile char data);
+    
+    // Sets the SPI clock speed under 400KHz
+    void slow_clock();
+    
+    // Sets the SPI clock speed to be fast
+    void fast_clock();
+    
+    byte spi_err; // SPI timeout flag, must be cleared manually
+};
+
+#endif // SPI_H
