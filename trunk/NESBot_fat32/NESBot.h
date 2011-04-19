c94acfc1e566e204aced02db969864f39d188896
pjgat09@gmail.com <pjgat09@gmail.com@7f936ef5-24ef-13a7-5a25-7df7dec640b6>
2011-04-19 14:24:33 +0000

    Added a working directory for a FAT16/32 based bot, and removed the unecessary extra directories

---
+++ trunk/NESBot_fat32/NESBot.h
@@ -0,0 +1,38 @@
+#ifdef BB
+
+#define RIGHT 10
+#define LEFT 9
+#define DOWN 8
+#define UP 7
+#define START 6
+#define SELECT 5
+#define B 4
+#define A 3
+
+#define READY 16    // analog pin 2
+#define GOBTN 17    // analog pin 3
+#define STATUS 14   // analog pin 0
+#define SD_LOAD 18  // analog pin 4
+
+#endif
+
+#ifdef SMD
+#define RED 5
+#define GREEN 6
+#define BLUE 7
+#define BTN1 8
+#define BTN2 9
+#define SELECT 3
+#define B 4
+#define START 14
+#define RIGHT 15
+#define LEFT 16
+#define DOWN 17
+#define UP 18
+#define A 19
+
+#define READY 6    // analog pin 2
+#define GOBTN 8    // analog pin 3
+#define STATUS 5   // analog pin 0
+#define SD_LOAD 9  // analog pin 4
+#endif
