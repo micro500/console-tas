0d15dbfa39fb037de7d537038bb46fedb1d7cb3f
pjgat09@gmail.com <pjgat09@gmail.com@7f936ef5-24ef-13a7-5a25-7df7dec640b6>
2014-01-03 02:40:00 +0000

    N64Bot: Cleaned up the python script output a bit

--- trunk/N64Bot/M64Stream.py
+++ trunk/N64Bot/M64Stream.py
@@ -4,9 +4,8 @@ def reverse_buttons(str):
     for i in range(0,len(str)/4):
         result = result + str[i*4 + 3] + str[i*4 + 2] + str[i*4 + 1] + str[i*4]
     return result
-        
 
-port = serial.Serial("COM10", 19200)
+port = serial.Serial("COM11", 19200)
 port.readline();
 
 movie = open("snark,kyman,sonicpacker,mickey_vis,tot-sm64.m64","rb")
@@ -34,24 +33,25 @@ controller_flags = movie.read(4)
 
 dummy = movie.read(160)
 
-rom_name = movie.read(32)
+rom_name = movie.read(32).rstrip('\0')
 rom_crc = movie.read(4)
 rom_country_code = movie.read(2)
 
 dummy = movie.read(56)
 
-video_plugin = movie.read(64)
-sound_plugin = movie.read(64)
-input_plugin = movie.read(64)
-rsp_plugin = movie.read(64)
+video_plugin = movie.read(64).rstrip('\0')
+sound_plugin = movie.read(64).rstrip('\0')
+input_plugin = movie.read(64).rstrip('\0')
+rsp_plugin = movie.read(64).rstrip('\0')
 
-author = movie.read(222)
-movie_description = movie.read(256)
+author = movie.read(222).rstrip('\0')
+movie_description = movie.read(256).rstrip('\0')
 
-print rom_name
-print author
-print movie_description
-print "Rerecords: " + str(rerecords)
+print "Rom:\t\t" + rom_name
+print "Author:\t\t" + author
+print "Description:\t" + movie_description
+print "Rerecords:\t" + str(rerecords)
+print "\n"
 
 buttons = movie.read(512)
 buttons = reverse_buttons(buttons)
@@ -59,30 +59,30 @@ buttons = reverse_buttons(buttons)
 port.write(buttons)
 port.flush()
 
-cur_frame = 0
+cur_frame = -1
 new_frame = 0
 done = 0
-print cur_frame
 while 1:
     new_string = port.readline()
     new_frame = int(new_string) / 4
     
     if new_frame != cur_frame:
         cur_frame = new_frame
-        print cur_frame
+        # Clear the line
+        print "\r\t\t\t\t",
+        print "\rFrame: " + str(cur_frame),
         
         if done == 0:
             if new_frame % 128 == 1:
-                print "sending..."
+                print "\tSending...",
                 buttons = movie.read(512)
                 if buttons != "":
                     buttons = reverse_buttons(buttons)
                     port.write(buttons)
-                    port.flush()
                 else:
                     done = 1
-                    print "Movie done!"
+                    print "\tAll data sent!"
                 if len(buttons) < 512:
                     done = 1
-                    print "Movie done!"
+                    print "\tAll data sent!"
 
